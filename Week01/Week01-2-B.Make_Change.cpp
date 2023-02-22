ed08baa98a1692c571b80aa9e1e3d60ce2d75b34
YuLin Liu <114201535+Ignition610@users.noreply.github.com>
2023-02-23 00:27:24 +0800

    Update Week01-2-B.Make_Change.cpp

--- Week01/Week01-2-B.Make_Change.cpp
+++ Week01/Week01-2-B.Make_Change.cpp
@@ -10,3 +10,14 @@ int main()
 	c=m-(50*a)-(5*b);
 	printf("%d=50*%d+5*%d+1*%d\n",m,a,b,c);
 }
+
+/*
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	
+	printf("%d=50*%d+5*%d+1*%d\n",a,a/50,a%50/5,a%5/1);
+}
+*/
