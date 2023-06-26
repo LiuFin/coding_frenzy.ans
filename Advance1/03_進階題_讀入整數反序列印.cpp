2c2433d50bac8e314428f29bf0c209c58279ea3a
YuLin Liu <114201535+LiuFin@users.noreply.github.com>
2023-06-26 18:56:36 +0800

    Update 03_進階題_讀入整數反序列印.cpp

--- Advance1/03_進階題_讀入整數反序列印.cpp
+++ Advance1/03_進階題_讀入整數反序列印.cpp
@@ -1,15 +1,14 @@
-///�]�p�@�ӵ{���A�ӵ{���i�H�s��Ū�J�����(��J0���ܵ����A�ܦh���W�L10�ӥ����)�A����N�ҿ�J������ƥH�ۤϧ���ܦb�e���W�C
-///�Ʀr�d��G��� 1 �V 1000
+///設計一個程式，該程式可以連續讀入正整數(輸入0表示結束，至多不超過10個正整數)，之後將所輸入的正整數以相反序顯示在畫面上。
+///數字範圍：整數 1 – 1000
 #include <stdio.h>
-int main()
-{
-	int a[10],i=0,b=1;
-	while(b)
-	{
-		scanf("%d",&b);
-		if(b!=0)a[i++]=b;
+int main(){
+	int a[10],n=0;
+	for(int i=0;i<10;i++){
+		scanf("%d",&a[i]);
+		if(a[i]==0) break;
+		n++;
 	}
-	while(i--){
+	for(int i=n-1;i>=0;i--){
 		printf("%d ",a[i]);
 	}
 	printf("\n");
